970594b9635d8b557d4fba93e3496873a1f0d8e0
Daniel Cloud <dcloud@sunlightfoundation.com>
2015-04-12 23:26:24 -0400

    Set up game code in prepr for trig stuff.

---
+++ README.md
@@ -0,0 +1,3 @@
+#TrigBlaster
+
+Just going through a [tutorial on SpriteKit and Swift](http://www.raywenderlich.com/90520/trigonometry-games-sprite-kit-swift-tutorial-part-1)
--- TrigBlaster/GameScene.swift
+++ TrigBlaster/GameScene.swift
@@ -7,39 +7,92 @@
 //
 
 import SpriteKit
+import CoreMotion
+
+let MaxPlayerAcceleration: CGFloat = 400
+let MaxPlayerSpeed: CGFloat = 200
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+
+    let playerSprite = SKSpriteNode(imageNamed: "Player")
+    var playerAcceleration = CGVector(dx: 0, dy: 0)
+    var playerVelocity = CGVector(dx: 0, dy: 0)
+
+    var accelerometerX: UIAccelerationValue = 0
+    var accelerometerY: UIAccelerationValue = 0
+
+    var lastUpdateTime: CFTimeInterval = 0
+
+    let motionManager = CMMotionManager()
+
+    deinit {
+        stopMonitoringAcceleration()
     }
-    
-    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch in (touches as! Set<UITouch>) {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+
+    override func didMoveToView(view: SKView) {
+        size = view.bounds.size
+
+        backgroundColor = SKColor(red: 94.0/255, green: 63.0/255, blue: 107.0/255, alpha: 1.0)
+
+        playerSprite.position = CGPoint(x: (size.width - playerSprite.size.width)/2, y: (size.height - playerSprite.size.height)/2)
+        addChild(playerSprite)
+
+        startMonitoringAcceleration()
+    }
+
+    override func update(currentTime: CFTimeInterval) {
+        let deltaTime = max(1.0/30, currentTime - lastUpdateTime)
+        lastUpdateTime = currentTime
+
+        updatePlayerAccelerationFromMotionManager()
+        updatePlayer(deltaTime)
+    }
+
+    func startMonitoringAcceleration() {
+
+        if motionManager.accelerometerAvailable {
+            motionManager.startAccelerometerUpdates()
+            NSLog("accelerometer updates on...")
         }
     }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+
+    func stopMonitoringAcceleration() {
+
+        if motionManager.accelerometerAvailable && motionManager.accelerometerActive {
+            motionManager.stopAccelerometerUpdates()
+            NSLog("accelerometer updates off...")
+        }
+    }
+
+    func updatePlayerAccelerationFromMotionManager() {
+        if let acceleration = motionManager.accelerometerData?.acceleration {
+
+            let filterFactor = 0.75
+
+            accelerometerX = acceleration.x * filterFactor + accelerometerX * (1 - filterFactor)
+            accelerometerY = acceleration.y * filterFactor + accelerometerY * (1 - filterFactor)
+
+            playerAcceleration.dx = CGFloat(accelerometerY) * -MaxPlayerAcceleration
+            playerAcceleration.dy = CGFloat(accelerometerX) * MaxPlayerAcceleration
+        }
+    }
+
+    func updatePlayer(dt: CFTimeInterval) {
+        playerVelocity.dx = playerVelocity.dx + playerAcceleration.dx * CGFloat(dt)
+        playerVelocity.dy = playerVelocity.dy + playerAcceleration.dy * CGFloat(dt)
+
+        playerVelocity.dx = max(-MaxPlayerSpeed, min(MaxPlayerSpeed, playerVelocity.dx))
+        playerVelocity.dy = max(-MaxPlayerSpeed, min(MaxPlayerSpeed, playerVelocity.dy))
+
+        var newX = playerSprite.position.x + playerVelocity.dx * CGFloat(dt)
+        var newY = playerSprite.position.y + playerVelocity.dy * CGFloat(dt)
+
+        newX = min(size.width, max(0, newX));
+        newY = min(size.height, max(0, newY));
+
+        println("newX: \(newX)")
+        println("newY: \(newY)")
+
+        playerSprite.position = CGPoint(x: newX, y: newY)
     }
 }
