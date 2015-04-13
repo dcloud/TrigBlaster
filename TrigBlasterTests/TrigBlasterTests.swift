d8138e13bcaf8e753ead24ec0eafaeeb1a3c325b
Daniel Cloud <dcloud@sunlightfoundation.com>
2015-04-12 21:54:35 -0400

    Initial Commit

---
+++ TrigBlasterTests/TrigBlasterTests.swift
@@ -0,0 +1,36 @@
+//
+//  TrigBlasterTests.swift
+//  TrigBlasterTests
+//
+//  Created by Daniel Cloud on 4/12/15.
+//  Copyright (c) 2015 Daniel Cloud. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TrigBlasterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
